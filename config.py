ec33ed7e759014a71fd34a8c1b3431bbd2f53813
Yaroslaw Klymenko <yaroslaw.klymko@gmail.com>
2019-07-24 00:52:33 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,18 @@
+token = "895692386:AAHx5XyR6rk36N9ZWfIsvs02x0sV0CBwEB4"
+
+create_log = True #BOOL-> printing logs to console, also may print to file
+
+users_file = "users.json" # saves list of unique users, stop logging  
+log_file = "log.txt" #CASE NONE - dont log to file 
+users_login_file = "logins.json" #file with users and their logins
+
+bot_commands="""start - press start
+                help - for more info
+                contacts - if you found a bug
+                cat - haha, classic 
+                """
+
+
+# URLS
+
+url_atb = "https://www.atbmarket.com/hot/akcii/economy/"
---
+++ readme.txt
@@ -0,0 +1,4 @@
+ first install pip
+ in cmd install libraries:
+ pip install requests
+ pip install pyTelegramBotAPI
